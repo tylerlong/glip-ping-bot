e3dc21815546f9f2c3be58c5dec18eadbc8b0b85
Tyler Long <tyler4long@gmail.com>
2018-01-24 12:50:32 +0800

    Create WebHook manually

--- index.js
+++ index.js
@@ -26,6 +26,25 @@ app.get('/oauth', async (req, res) => {
     await rc.authorize({ code: req.query.code, redirect_uri: `${process.env.GLIP_BOT_SERVER}/oauth` })
   } catch (e) {
     console.error(e.response.data)
+    return
+  }
+  const requestData = {
+    eventFilters: [
+      '/restapi/v1.0/glip/posts',
+      '/restapi/v1.0/glip/groups',
+      '/restapi/v1.0/subscription/~?threshold=60&interval=15'
+    ],
+    deliveryMode: {
+      transportType: 'WebHook',
+      address: `${process.env.GLIP_BOT_SERVER}/webhook`,
+      verificationToken: 'xelXMLMhKDLVyViTy74d6CZ7Y7hJIqbS'
+    },
+    expiresIn: 86400 // 1 days
+  }
+  try {
+    await rc.post('/restapi/v1.0/subscription', requestData)
+  } catch (e) {
+    console.error(e)
   }
   fs.writeFileSync(tokenFile, JSON.stringify(rc.token())) // save token
   res.send('')
